60eda7a465452cba78d6809dba12e9af0a8abe97
Javier Garcia <jagarcia29@gmail.com>
2018-01-22 21:03:59 -0500

    fixed string format of RSVP and PARTSTAT within Attendees

--- src/utils/set-contact.js
+++ src/utils/set-contact.js
@@ -1,9 +1,8 @@
 export default function setContact({ name, email, rsvp, partStat }) {
     let formattedAttendee = 'CN='
     formattedAttendee += name || 'Unnamed attendee'
-    formattedAttendee += '";';
-    formattedAttendee += rsvp ? 'RSVP=TRUE:' : '';
-    formattedAttendee += partStat ? ':PARSTSTAT='+partStat : '';
+    formattedAttendee += rsvp ? ';RSVP=TRUE' : '';
+    formattedAttendee += partStat ? ';PARSTSTAT='+partStat : '';
     formattedAttendee += email ? `:mailto:${email}` : ''
 
   return formattedAttendee
